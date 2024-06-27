776be8d6d81267962657b8171a793ef36d23e8c4
TechRam09 <nsshreeram@gmail.com>
2024-06-27 23:27:09 +0530

    Update MovieList.jsx

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDataContext } from "../../context/dataContext";
 
 function MovieList() {
+  const { movieList, setMovieList } = useDataContext();
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, [type]);
+
   return <div>MovieList</div>;
 }
 
