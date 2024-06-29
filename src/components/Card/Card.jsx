aa249e88c3298784857038e76b930f5558656e87
TechRam09 <nsshreeram@gmail.com>
2024-06-29 23:56:34 +0530

    Update Card.jsx
    Added useEffect hook

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,11 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 function Card({ movie }) {
   const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
 
   return <></>;
 }
