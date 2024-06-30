46fe58d5927382d39650e141df8ea611cb07e151
TechRam09 <nsshreeram@gmail.com>
2024-06-30 23:26:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,14 @@ import {
 import Layout from "./Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
 import { DataProvider } from "./context/dataContext.jsx";
+import MovieList from "./components/movieList/MovieList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="movie/:id" element={<h1>Movie detail page</h1>} />
-      <Route path="movies/:type" element={<h1>Movie list page</h1>} />
+      <Route path="movies/:type" element={<MovieList />} />
       <Route path="/*" element={<h1>404 Page not Found</h1>} />
     </Route>
   )
