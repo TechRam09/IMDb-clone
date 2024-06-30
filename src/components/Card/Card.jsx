160b476fc3fc7a7672fc98495cafda1ca0d62ef9
TechRam09 <nsshreeram@gmail.com>
2024-06-30 23:25:05 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,12 @@
+// Card.jsx
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function Card({ movie }) {
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
@@ -11,15 +15,48 @@ function Card({ movie }) {
 
   return (
     <>
-      {
-        isLoading
-        ?
-        <div>
-          <SkeletonTheme color="#202020" highlightColor="#444">
-            <Skeleton height={300} duration={2} />
+      {isLoading ? (
+        <div className="cards mx-1">
+          <SkeletonTheme baseColor="#202020" highlightColor="#444">
+            <Skeleton height={300} width={200} duration={3} />
           </SkeletonTheme>
         </div>
-      }
+      ) : (
+        <Link to={`/movie/${movie.id}`}>
+          <div
+            className="cards inline-block relative transition-transform duration-[2000] overflow-hidden m-[0.19rem] rounded-[10px] h-[300px] min-w-[200px] cursor-pointer z-0 hover:z-[1000] hover:scale-[1.2]"
+            style={{ border: "1px solid rgb(99, 99, 99)" }}
+          >
+            <img
+              className="h-[300px]"
+              src={`https://image.tmdb.org/t/p/original${
+                movie ? movie.poster_path : ""
+              }`}
+              alt="poster"
+            />
+            <div
+              className="card_overlay absolute bottom-0 px-4 pb-4 h-[290px] w-full flex flex-col justify-end opacity-0 transition-opacity hover:opacity-100 duration-[2000]"
+              style={{
+                backgroundImage: "linear-gradient(rgb(0,0,0,0), rgb(0,0,0,1))",
+              }}
+            >
+              <div className="font-black text-base mb-[0.4rem]">
+                {movie ? movie.original_title : ""}
+              </div>
+              <div className="cards_runtime mb-1 text-xs">
+                {movie ? movie.release_date : ""}
+                <span className="card_rating float-right">
+                  {movie ? movie.vote_average.toFixed(2) : ""}
+                  <i className="fas fa-star" />
+                </span>
+              </div>
+              <div className="card_description mb-1 text-xs italic">
+                {movie ? movie.overview.slice(0, 118) + "..." : ""}
+              </div>
+            </div>
+          </div>
+        </Link>
+      )}
     </>
   );
 }
