6cce23f1faaab48f4000e10390ee24f5eba40da2
TechRam09 <nsshreeram@gmail.com>
2024-06-30 23:26:50 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useDataContext } from "../../context/dataContext";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/MovieList";
 
 function Home() {
   const apiKey = import.meta.env.VITE_API_KEY;
@@ -18,7 +19,7 @@ function Home() {
         setPopular(data.results);
       })
       .catch((error) => console.error("Fetching error: ", error));
-  }, []);
+  }, [apiKey, setPopular]); // Ensure dependencies are correct
 
   return (
     <>
@@ -28,7 +29,7 @@ function Home() {
           infiniteLoop={true}
           showStatus={false}
           showThumbs={false}
-          transitionTime={3}
+          transitionTime={4}
         >
           {popular.map((movie) => (
             <Link
@@ -42,7 +43,7 @@ function Home() {
                     movie && movie.backdrop_path
                   }}`}
                   alt="movie_image"
-                  className=" m-auto w-full block"
+                  className="m-auto w-full block"
                 />
               </div>
               <div className="posterImage_overlay absolute bottom-0 p-20 h-[70%] flex flex-col items-start justify-end w-full opacity-100 hover:opacity-100">
@@ -52,7 +53,7 @@ function Home() {
                 <div className="posterImage_runtime text-3xl mb-4">
                   {movie ? movie.release_date : ""}
                   <span className="posterImage__rating ml-4">
-                    {movie ? movie.vote_average : ""}
+                    {movie ? movie.vote_average.toFixed(2) : ""}
                     <i className="fas fa-star" />{" "}
                   </span>
                 </div>
@@ -64,6 +65,7 @@ function Home() {
           ))}
         </Carousel>
       </div>
+      <MovieList />
     </>
   );
 }
