28cb4676f9260302d11664cba31ac0a4b6d4b9ec
TechRam09 <nsshreeram@gmail.com>
2024-07-04 23:21:25 +0530

    Update main.jsx
    Add element to render for movie detail page

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,13 @@ import Layout from "./Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
 import { DataProvider } from "./context/dataContext.jsx";
 import MovieList from "./components/movieList/MovieList.jsx";
+import MovieDetail from "./pages/movieDetail/MovieDetail.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="movie/:id" element={<h1>Movie detail page</h1>} />
+      <Route path="movie/:id" element={<MovieDetail />} />
       <Route path="movies/:type" element={<MovieList />} />
       <Route path="/*" element={<h1>404 Page not Found</h1>} />
     </Route>
