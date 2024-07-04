25a131573e71ff4f3e15973eae37bf11787194ad
TechRam09 <nsshreeram@gmail.com>
2024-07-04 23:17:50 +0530

    Update MovieDetail.jsx
    Adde functionality for fetching movie details

--- src/pages/movieDetail/MovieDetail.jsx
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./movie.css";
 
 function MovieDetail() {
-  return <div>MovieDetail</div>;
+  const [movieDetail, setMovieDetail] = useState();
+  const { id } = useParams();
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    getData();
+    window.scrollTo(0, 0);
+  }, []);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        id ? id : ""
+      }?api_key=${apiKey}&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovieDetail(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return <h2>s</h2>;
 }
 
 export default MovieDetail;
